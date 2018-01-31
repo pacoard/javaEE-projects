242ebef7de8cdc30224d3d4726daf4df7770c237
pacoard <pacoard@gmail.com>
2018-01-31 17:20:22 -0600

    Refactor

---
+++ assignment1/src/edu/iit/itmd515/client/GreetingServiceAsync.java
@@ -0,0 +1,10 @@
+package edu.iit.itmd515.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+
+/**
+ * The async counterpart of <code>GreetingService</code>.
+ */
+public interface GreetingServiceAsync {
+	void greetServer(String input, AsyncCallback<String> callback) throws IllegalArgumentException;
+}

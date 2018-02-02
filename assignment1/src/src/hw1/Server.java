ea6edacaca589d5d55a4785be19343b15f4600ca
kelu22 <raquel.noblejas.sampedro@gmail.com>
2018-02-01 19:36:54 -0600

    Multithreading implemented

--- assignment1/src/src/hw1/Server.java
+++ assignment1/src/src/hw1/Server.java
@@ -8,36 +8,43 @@ import java.io.PrintWriter;
 import java.net.*;
 import java.util.Scanner;
 
-public class Server {
-    public static void main(String[] args) throws IOException {
-
-        try ( 
-            ServerSocket serverSocket = new ServerSocket(4444);
-            Socket clientSocket = serverSocket.accept();
-            PrintWriter out =
-                new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(
-                new InputStreamReader(clientSocket.getInputStream()));
-        ) {
-        
-            String inputLine;
-            String outputLine;
-            do{
-	            // Initiate conversation with client
-	            System.out.println("Client connected");
-	            String operation = in.readLine();
-	            System.out.println("The request from client is:" + operation);
-	            //Process the operation
-	            double opResult = Server.calculate(operation);
-	            System.out.println("The operation result is: " + opResult);
-	            out.println("The operation result is: " + opResult);
-            }while (!in.equals("exit"));
-        } catch (IOException e) {
-            System.out.println("Exception caught when trying to listen on port 4444");
-            System.out.println(e.getMessage());
-        } catch (Exception e){
-        	System.out.println("Server error:"+e.getMessage());
-        }
+public class Server extends Thread{
+	private final static int serverPort   = 4444;
+	protected Thread runningThread= null;
+	private Socket clientSocket;
+	private static ServerSocket serverSocket;
+	
+	public Server(Socket socket){
+		this.clientSocket= socket;
+	}
+	
+	public void run(){
+        try{  
+            //ServerSocket serverSocket = new ServerSocket(4444);
+	            //Socket clientSocket = serverSocket.accept();
+	            PrintWriter out =
+	                new PrintWriter(clientSocket.getOutputStream(), true);
+	            BufferedReader in = new BufferedReader(
+	                new InputStreamReader(clientSocket.getInputStream()));
+		        
+	            String inputLine;
+	            String outputLine;
+	            do{
+		            // Initiate conversation with client
+		            System.out.println("Client connected");
+		            String operation = in.readLine();
+		            System.out.println("515OK -->The request from client is:" + operation);
+		            //Process the operation
+		            double opResult = Server.calculate(operation);
+		            System.out.println("The operation result is: " + opResult);
+		            out.println("The operation result is: " + opResult);
+	            }while (!in.equals("exit"));
+	        } catch (IOException e) {
+	            System.out.println("Exception caught when trying to listen on port 4444");
+	            System.out.println(e.getMessage());
+	        } catch (Exception e){
+	        	System.out.println("Server error:"+e.getMessage());
+	        }
     }
     
     public static double calculate(String operation){
@@ -77,6 +84,25 @@ public class Server {
     		result = a%b;
         }
     	return result;
+    }
+    
+    public static void main(String[] args) throws IOException {
+    	try {
+			serverSocket = new ServerSocket(serverPort);
+			System.out.println("Server running on port "+ serverPort);
+    	} catch (IOException e) {
+            System.out.println("Exception caught when trying to listen on port 4444");
+            System.out.println(e.getMessage());
+        } 
+    	
+    	while(true){
+    		try{
+	            Socket clientSocket = serverSocket.accept();
+	            new Server(clientSocket).start();
+	        } catch (Exception e){
+	        	System.out.println("Server error:"+e.getMessage());
+	        }
+    	}
     }
 }
 

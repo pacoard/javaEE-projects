d1a2490e52d1c533e29adc8a030be2001b3e27f5
kelu22 <raquel.noblejas.sampedro@gmail.com>
2018-03-04 14:07:12 -0600

    Trip DAO implemented

---
+++ project/src/main/java/edu/iit/itmd515/model/TripDAO.java
@@ -0,0 +1,59 @@
+package edu.iit.itmd515.model;
+
+import java.util.List;
+
+/**
+ * Interface to Trips database
+ * 
+ * @author Francisco, Raquel
+ *
+ */
+public interface TripDAO {
+
+	/**
+	 * Create method
+	 * 
+	 * @param consumerId
+	 * @param driverId
+	 * @param distance
+	 * @param dropoffLocation
+	 * @param pickupLocation
+	 * @param price
+	 * @return created Trip
+	 */
+	public Trip createTrip(Long consumerId, Long driverId, double distance, String dropoffLocation, String pickupLocation,
+			double price);
+	
+	/**
+	 * Read method
+	 * 
+	 * @return List of Trips
+	 */
+	public List<Trip> getAllTrips();
+	
+	/**
+	 * Update method
+	 * 
+	 * @param consumerId
+	 * @param driverId
+	 * @param distance
+	 * @param dropoffLocation
+	 * @param pickupLocation
+	 * @param price
+	 * @return updated Trip
+	 */
+	public void updateTrip(Trip t);
+	
+	/**
+	 *  Delete all method
+	 */
+	public void deleteAllTrips();
+	
+	/**
+	 * Delete by ID method
+	 * 
+	 * @param id
+	 */
+	public void deleteTripsById(Long id);
+}
+
---
+++ project/src/main/java/edu/iit/itmd515/model/TripDAOImpl.java
@@ -0,0 +1,57 @@
+package edu.iit.itmd515.model;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+/**
+ * TripDAO methods implementation
+ * 
+ * @author Francisco, Raquel
+ *
+ */
+
+public class TripDAOImpl implements TripDAO {
+
+	// Singleton 
+	private static TripDAOImpl instance;
+	private TripDAOImpl () {}
+	public static TripDAOImpl getInstance() {
+		if (instance == null)
+			instance = new TripDAOImpl();
+		return instance;
+	}
+	
+	public Trip createTrip(Long consumerId, Long driverId, double distance, String dropoffLocation, String pickupLocation,
+			double price) {
+		EntityManager em = EMFService.get().createEntityManager();
+		Trip t = new Trip(consumerId, driverId, distance, dropoffLocation, pickupLocation, price);
+		em.persist(t);
+		em.close();
+		return t;
+	}
+	public List<Trip> getAllTrips() {
+		EntityManager em = EMFService.get().createEntityManager();
+		Query q = em.createQuery("SELECT m FROM Trips m");
+		List<Trip> ts = q.getResultList();
+		em.close();
+		return ts;
+	}
+	public void updateTrip(Trip t) {
+		EntityManager em = EMFService.get().createEntityManager();
+		em.merge(t);
+		em.close();
+	}
+	public void deleteAllTrips() {
+		EntityManager em = EMFService.get().createEntityManager();
+		em.createQuery("DELETE FROM Trips e").executeUpdate();
+		em.close();
+	}
+	public void deleteTripsById(Long id) {
+		EntityManager em = EMFService.get().createEntityManager();
+		Trip t = em.find(Trip.class, id);
+		em.remove(t);
+		em.close();
+	}
+
+}

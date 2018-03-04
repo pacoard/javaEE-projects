1e958548cbac04ce1f53a899a7fde09c6cb0f5a5
kelu22 <raquel.noblejas.sampedro@gmail.com>
2018-03-04 13:47:05 -0600

    Car DAO implementation

---
+++ project/src/main/java/edu/iit/itmd515/model/CarDAO.java
@@ -0,0 +1,56 @@
+package edu.iit.itmd515.model;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+/**
+ * CarDAO methods 
+ * 
+ * @author Francisco, Raquel
+ *
+ */
+
+public interface CarDAO {
+	/**
+	 * Create method
+	 * 
+	 * @param brand
+	 * @param nSeats
+	 * @param color
+	 * @param plate
+	 * @return created Car
+	 */
+	public Car createCar(Long driverId, String brand, int nSeats, String color, String plate);
+	
+	/**
+	 * Read method
+	 * 
+	 * @return List of Cars
+	 */
+	public List<Car> getAllCars();
+	
+	/**
+	 * Update method
+	 * 
+	 * @param brand
+	 * @param nSeats
+	 * @param color
+	 * @param plate
+	 * @return updated Car
+	 */
+	public void updateCar(Car c);
+	
+	/**
+	 *  Delete all method
+	 */
+	public void deleteAllCars();
+	
+	/**
+	 * Delete by ID method
+	 * 
+	 * @param id
+	 */
+	public void deleteCarById(Long id);
+
+}
---
+++ project/src/main/java/edu/iit/itmd515/model/CarDAOImpl.java
@@ -0,0 +1,55 @@
+package edu.iit.itmd515.model;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+/**
+ * CarDAO methods implementation
+ * 
+ * @author Francisco, Raquel
+ *
+ */
+
+public class CarDAOImpl implements CarDAO{
+	// Singleton 
+		private static CarDAOImpl instance;
+		private CarDAOImpl () {}
+		public static CarDAOImpl getInstance() {
+			if (instance == null)
+				instance = new CarDAOImpl();
+			return instance;
+		}
+		
+		public Car createCar(Long driverId, String brand, int nSeats, String color, String plate) {
+			EntityManager em = EMFService.get().createEntityManager();
+			Car c = new Car(driverId, brand, nSeats, color, plate); //We have to insert driverID
+			em.persist(c);
+			em.close();
+			return c;
+		}
+		public List<Car> getAllCars() {
+			EntityManager em = EMFService.get().createEntityManager();
+			Query q = em.createQuery("SELECT m FROM Cars m");
+			List<Car> cs = q.getResultList();
+			em.close();
+			return cs;
+		}
+		public void updateCar(Car c) {
+			EntityManager em = EMFService.get().createEntityManager();
+			em.merge(c);
+			em.close();
+		}
+		public void deleteAllCars() {
+			EntityManager em = EMFService.get().createEntityManager();
+			em.createQuery("DELETE FROM CArs e").executeUpdate();
+			em.close();
+		}
+		public void deleteCarById(Long id) {
+			EntityManager em = EMFService.get().createEntityManager();
+			Car c = em.find(Car.class, id);
+			em.remove(c);
+			em.close();
+		}
+		
+}

6d3d428f7630f992fb442a4755ed0dc8c037b9cb
kelu22 <raquel.noblejas.sampedro@gmail.com>
2018-03-30 18:53:52 -0500

    Administrator DAO interface created

---
+++ project/src/main/java/edu/iit/itmd515/model/AdministratorDAO.java
@@ -0,0 +1,61 @@
+package edu.iit.itmd515.model;
+
+import java.util.List;
+
+/**
+ * AdministratorDAO methods 
+ * 
+ * @author Francisco, Raquel
+ *
+ */
+public interface AdministratorDAO {
+	/**
+	 * Create method
+	 * 
+	 * @param email
+	 * @param password
+	 * @param List<Consumer>
+	 * @param List<Driver>
+	 * @return created Administrator
+	 */
+	public Administrator createAdministrator(String email, String password, List<Consumer> consumers,List<Driver> drivers);
+	
+	/**
+	 * Read method
+	 * 
+	 * @return List of Banned consumers
+	 */
+	
+	public List<Consumer> getBannedConsumers();
+	/**
+	 * Read method
+	 * 
+	 * @return List of Banned drivers
+	 */
+	public List<Driver> getBannedDrivers();
+	
+	/**
+	 * Write method to delete a banned consumer
+	 * 
+	 */
+	public void deleteConsumer(Consumer consumer);
+	
+	/**
+	 * Write method to delete a suspended driver
+	 * 
+	 */
+	public void deleteDriver(Driver driver);
+	
+	/**
+	 * Write method to insert new banned consumer
+	 * 
+	 */
+	public void banConsumer(Consumer consumer);
+	
+	/**
+	 * Write method to insert new suspended driver
+	 * 
+	 */
+	public void suspendDriver(Driver driver);
+	
+}
